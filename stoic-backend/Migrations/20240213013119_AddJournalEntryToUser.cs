e28137ddc9b88caed66544d8bf206a6b3d24bd76
Jon <jonseddon@clear-launch.com>
2024-02-12 19:31:47 -0600

    add JournalEntry Migration

---
+++ stoic-backend/Migrations/20240213013119_AddJournalEntryToUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace stoic_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJournalEntryToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JournalEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Answer1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer3 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer4 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EntryDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JournalEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JournalEntries_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JournalEntries_UserId",
+                table: "JournalEntries",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JournalEntries");
+        }
+    }
+}
