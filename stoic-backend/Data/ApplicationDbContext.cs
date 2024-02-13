295460406498ac68fa177a043965a2e4ea2b8748
Jon <jonseddon@clear-launch.com>
2024-02-12 19:30:39 -0600

    add JournalEntry model

--- stoic-backend/Data/ApplicationDbContext.cs
+++ stoic-backend/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using stoic_backend.Models;
 
 namespace stoic_backend.Data
 {
@@ -8,6 +9,18 @@ namespace stoic_backend.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<JournalEntry>()
+                .HasOne(e => e.User)
+                .WithMany()
+                .HasForeignKey(e => e.UserId);
         }
     }
 }
---
+++ stoic-backend/Models/JournalEntry.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace stoic_backend.Models
+{
+    public class JournalEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; } 
+        public string Answer3 { get; set; } 
+        public string Answer4 { get; set; }
+        public string Notes { get; set; }
+        public DateTime EntryDate { get; set; }
+        public IdentityUser User { get; set; }
+
+    }
+}
